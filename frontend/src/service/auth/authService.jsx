40146545b715fcd5b9a8c840e69233c3b5c5014d
ihebkh <khmiri.iheb@esprit.tn>
2025-05-31 08:55:44 +0100

    cv parsing

--- frontend/src/service/auth/authService.jsx
+++ frontend/src/service/auth/authService.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const api_url = 'http://localhost:8000';
+
 export async function login(email, password) {
   try {
-    const res = await axios.post('http://localhost:8000/auth/login', { email, password });
+    const res = await axios.post(`${api_url}/auth/login`, { email, password });
     return res.data;
   } catch (err) {
     throw new Error(err.response?.data?.detail || 'Erreur serveur');
